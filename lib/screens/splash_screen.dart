ea2f0e381c2b424ef6592fcd50c5418bd849d5ea
Haram0111 <5gkfka5@naver.com>
2026-06-15 23:06:17 +0900

    feat: 자동로그인 유지

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,9 +1,10 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_svg/flutter_svg.dart'; // flutter_svg 패키지 import
-
+import 'package:flutter_svg/flutter_svg.dart';
+import '../../core/auth/token_storage.dart';
+import 'my_library/my_library_page.dart';
 import 'onboarding/onboarding_flow_screen.dart';
+
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -13,28 +14,49 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
   @override
   void initState() {
     super.initState();
+    _moveNextPage();
+  }
 
-    Timer(const Duration(seconds: 2), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (_) => const OnboardingFlowScreen()),
+  Future<void> _moveNextPage() async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    final accessToken = await TokenStorage.getAccessToken();
+
+    if (!mounted) return;
+
+    if (accessToken != null && accessToken.isNotEmpty) {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+          builder: (_) => const MyLibraryPage(),
+        ),
+            (route) => false,
       );
-    });
+    } else {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+          builder: (_) => const OnboardingFlowScreen(),
+        ),
+            (route) => false,
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return AnnotatedRegion<SystemUiOverlayStyle>(
-      // 배경이 흰색이므로 상태바 아이콘(배터리, 시간 등)은 검은색(dark)으로 설정
       value: SystemUiOverlayStyle.dark,
       child: Scaffold(
-        backgroundColor: Colors.white, // 배경색 흰색으로 변경
+        backgroundColor: Colors.white,
         body: Center(
           child: SvgPicture.asset(
-            'assets/images/logo.svg', // 로고 경로
-            width: 200, // 로고 크기 조절 (필요에 따라 숫자 변경)
+            'assets/images/logo.svg',
+            width: 200,
           ),
         ),
       ),
